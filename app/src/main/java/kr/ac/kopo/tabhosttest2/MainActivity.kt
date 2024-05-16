20c95fca7b76dd632196d7d82d352fbcbf410716
starchaser777 <les980730@gmail.com>
2024-05-16 14:08:21 +0900

    ActionBar 적용

--- app/src/main/java/kr/ac/kopo/tabhosttest2/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/tabhosttest2/MainActivity.kt
@@ -55,19 +55,11 @@ class MainActivity : AppCompatActivity(), ActionBar.TabListener {
     }
 
     override fun onTabUnselected(tab: ActionBar.Tab?, ft: FragmentTransaction?) {
-<<<<<<< HEAD
 
     }
 
     override fun onTabReselected(tab: ActionBar.Tab?, ft: FragmentTransaction?) {
 
-=======
-        TODO("Not yet implemented")
-    }
-
-    override fun onTabReselected(tab: ActionBar.Tab?, ft: FragmentTransaction?) {
-        TODO("Not yet implemented")
->>>>>>> origin/master
     }
     // 탭을 선택하면 변경되는 Content로 Fragment를 사용
     class MyFragment : Fragment() {
